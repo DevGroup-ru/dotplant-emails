dca8c38a58ebae9fe8ba33af8300178988b97e81
Радькова Ольга <olga@devgroup.ru>
2017-02-15 14:37:03 +0300

    fix multilingual messages

--- src/messages/ru/dotplant.emails.php
+++ src/messages/ru/dotplant.emails.php
@@ -2,4 +2,16 @@
 
 return [
     'English text' => 'Русский текст',
+    'Emails' => 'Почта',
+    'Email' => 'Электронный адрес',
+    'Messages' => 'Сообщения',
+    'Templates' => 'Шаблоны',
+    'Template' => 'Шаблон',
+    'Status' => 'Статус',
+    'Template params' => 'Параметры шаблона',
+    'Update' => 'Обновление',
+    'Name' => 'Название',
+    'Subject view file' => 'Файл заголовка',
+    'Body view file' => 'Файл тела письма',
+    'Is active' => 'Активный'
 ];
